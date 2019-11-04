9c397b0c105ccd25562fc4bbf013ec2f64ed1640
xianliru <xianliru@100tal.com>
2019-11-04 17:25:04 +0800

    1 spring cloud alibaba sentinel 限流、熔断整合

--- cloud-gateway-zuul/src/main/java/com/xian/cloud/controller/RulesController.java
+++ cloud-gateway-zuul/src/main/java/com/xian/cloud/controller/RulesController.java
@@ -4,6 +4,7 @@ import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
 import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
 import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
 import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,16 +20,19 @@ import java.util.Set;
 public class RulesController {
 
 	@GetMapping("/api")
+	@SentinelResource("api")
 	public Set<ApiDefinition> apiRules() {
 		return GatewayApiDefinitionManager.getApiDefinitions();
 	}
 
 	@GetMapping("/gateway")
+	@SentinelResource("gateway")
 	public Set<GatewayFlowRule> apiGateway() {
 		return GatewayRuleManager.getRules();
 	}
 
 	@GetMapping("/flow")
+	@SentinelResource("flow")
 	public List<FlowRule> apiFlow() {
 		return FlowRuleManager.getRules();
 	}
