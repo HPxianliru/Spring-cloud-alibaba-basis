0dbd59cfae4adc9ca35c587394d72a37b0940294
xianliru <xianliru@100tal.com>
2019-11-09 11:03:03 +0800

    1 gateway 动态刷新路由

--- cloud-gateway/src/main/java/com/xian/cloud/controller/GatewayRoutesController.java
+++ cloud-gateway/src/main/java/com/xian/cloud/controller/GatewayRoutesController.java
@@ -1,12 +1,12 @@
 package com.xian.cloud.controller;
 
+import com.xian.cloud.event.DynamicRouteServiceImpl;
 import com.xian.cloud.event.RefreshRouteService;
 import com.xian.cloud.model.RestResult;
 import com.xian.cloud.model.RestResultBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <Description>
@@ -22,9 +22,58 @@ public class GatewayRoutesController {
     @Autowired
     private RefreshRouteService refreshRouteService;
 
+    @Autowired
+    private DynamicRouteServiceImpl dynamicRouteService;
+
     @GetMapping("/refreshRoutes")
     public RestResult refreshRoutes(){
         refreshRouteService.refreshRoutes();
         return RestResultBuilder.builder().success().build();
     }
+
+    /**
+     *
+     * @param definition
+     * @return
+     */
+    @RequestMapping(value = "routes/add",method = RequestMethod.POST)
+    public RestResult add(@RequestBody RouteDefinition definition){
+        boolean flag = dynamicRouteService.add(definition);
+        if(flag){
+            return RestResultBuilder.builder().success().build();
+        }
+        return RestResultBuilder.builder().failure().build();
+    }
+
+
+    /**
+     *
+     * @param definition
+     * @return
+     */
+    @RequestMapping(value = "routes/update",method = RequestMethod.POST)
+    public RestResult update(@RequestBody RouteDefinition definition){
+        boolean flag = dynamicRouteService.add(definition);
+        if(flag){
+            return RestResultBuilder.builder().success().build();
+        }
+        return RestResultBuilder.builder().failure().build();
+    }
+
+    /**
+     *
+     * @param serviceId
+     * @return
+     */
+    @RequestMapping(value = "routes/del",method = RequestMethod.POST)
+    public RestResult update(@RequestParam("serviceId") String serviceId){
+        boolean flag = dynamicRouteService.del(serviceId);
+        if(flag){
+            return RestResultBuilder.builder().success().build();
+        }
+        return RestResultBuilder.builder().failure().build();
+    }
+
+
+
 }
---
+++ cloud-gateway/src/main/java/com/xian/cloud/event/DynamicRouteServiceImpl.java
@@ -0,0 +1,76 @@
+package com.xian.cloud.event;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+/**
+ * <Description>
+ *
+ * @author xianliru@100tal.com
+ * @version 1.0
+ * @createDate 2019/11/09 10:40
+ */
+@Slf4j
+@Service
+public class DynamicRouteServiceImpl implements ApplicationEventPublisherAware {
+
+    @Qualifier("routeDefinitionRepositor")
+    @Autowired
+    private RouteDefinitionWriter routeDefinitionWriter;
+
+    private ApplicationEventPublisher publisher;
+
+
+    /**
+     * 添加路由实体类
+     * @param definition
+     * @return
+     */
+    public boolean add(RouteDefinition definition){
+        routeDefinitionWriter.save((Mono<RouteDefinition>) Mono.just(definition).subscribe());
+        this.publisher.publishEvent(new RefreshRoutesEvent(this));
+        return true;
+    }
+
+    /**
+     *
+     * @param definition 路由实体类
+     * @return
+     */
+    public boolean update(RouteDefinition definition){
+        try {
+            routeDefinitionWriter.delete(Mono.just(definition.getId()));
+        }catch (Exception e){
+            log.error("update 失败。没有找到对应的路由ID :{}",definition.getId());
+        }
+
+        routeDefinitionWriter.save((Mono<RouteDefinition>) (Mono.just(definition)).subscribe());
+        this.publisher.publishEvent(new RefreshRoutesEvent(this));
+        return true;
+    }
+
+    /**
+     * serviceId
+     * @param id
+     * @return
+     */
+    public boolean del(String id){
+        routeDefinitionWriter.delete(Mono.just(id));
+        this.publisher.publishEvent(new RefreshRoutesEvent(this));
+        return true;
+    }
+
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.publisher = applicationEventPublisher;
+    }
+}
